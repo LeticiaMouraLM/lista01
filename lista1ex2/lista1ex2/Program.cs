9028d91ac9ee0c41bfc5bf757f438e00fb4c9fe4
LeticiaMouraLM <lehs5900@gmail.com>
2026-04-02 22:11:13 -0300

    Add files via upload

---
+++ lista1ex2/lista1ex2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Deployment.Internal;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista1ex2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int b;
+            int h;
+            int a;
+
+            Console.WriteLine("informe a Base do retangulo");
+            b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("informe a altura do retangulo");
+            h = int.Parse(Console.ReadLine());
+
+            a = b * h; 
+
+            Console.WriteLine("A area do retangulo é");
+            Console.WriteLine(a);
+        }
+    }
+}         
+
+        
