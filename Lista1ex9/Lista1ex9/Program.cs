da74b20d774da7741ab90cc7b1549a8fc8bf7cae
LeticiaMouraLM <lehs5900@gmail.com>
2026-04-23 22:41:31 -0300

    Add files via upload

---
+++ Lista1ex9/Lista1ex9/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Lista1ex9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Diãmetro: ");
+            double d = double.Parse(Console.ReadLine());
+
+            double r = d / 2;
+            double area = Math.PI * r * r;
+
+            Console.WriteLine ("Área do circulo: " + area);
+
+
+        }
+    }
+}
