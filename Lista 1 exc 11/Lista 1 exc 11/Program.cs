6d37aa25fc5fbe0ee942bc2e651731e3b8a6dc9f
LeticiaMouraLM <lehs5900@gmail.com>
2026-04-23 22:52:32 -0300

    Add files via upload

---
+++ Lista 1 exc 11/Lista 1 exc 11/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Lista_1_exc_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("digite X: ");
+            double X = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite y: ");
+            double y = double.Parse(Console.ReadLine());
+
+            double resultado = Math.Pow (X, y);
+
+            Console.WriteLine("resultado: " + resultado);
+
+        }
+    }
+}
