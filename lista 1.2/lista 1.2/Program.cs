355eb6c87b9bd673f51efb845dd29bae8c8a2064
LeticiaMouraLM <lehs5900@gmail.com>
2026-04-23 21:42:53 -0300

    Add files via upload

---
+++ lista 1.2/lista 1.2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista_1._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+       
+
+         Console.Write("Digite o valor da aresta: ");
+         Double Aresta = Double.Parse(Console.ReadLine());
+
+         Double Area = Aresta * Aresta;
+
+         Console.WriteLine("Area do quadrado: " + Area);
+
+
+
+        }
+    }
+}
