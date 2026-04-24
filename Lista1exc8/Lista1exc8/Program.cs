fd22dfdbb18ac4b2bf9c02b0a6c26763f4817882
LeticiaMouraLM <lehs5900@gmail.com>
2026-04-23 22:34:02 -0300

    Add files via upload

---
+++ Lista1exc8/Lista1exc8/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1exc8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Temperatura em Celsius: ");
+            double c = double.Parse(Console.ReadLine());
+
+            double d = (c * 9 / 5) + 32;
+
+            Console.WriteLine("Fahrenheit: " + d);
+
+        }
+    }
+}
