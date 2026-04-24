b0ff9a0023e89475faf080bf564885df8e1aa136
LeticiaMouraLM <lehs5900@gmail.com>
2026-04-23 22:20:55 -0300

    Add files via upload

---
+++ Lista1exc6/Lista1exc6/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Lista1exc6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Valor 1: ");
+            double a = double.Parse(Console.ReadLine());
+
+            Console.Write("Valor 2: ");
+            double b = double.Parse(Console.ReadLine());
+
+            double media = Math.Sqrt( a * b );
+
+            Console.WriteLine("Media geométrica: " + media);
+        }
+    }
+}
