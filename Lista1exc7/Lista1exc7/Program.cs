83402dcacdb1f8fa0f344babe82401793ef42a33
LeticiaMouraLM <lehs5900@gmail.com>
2026-04-23 22:26:46 -0300

    Add files via upload

---
+++ Lista1exc7/Lista1exc7/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1exc7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Milhas marítimas: ");
+            double milhas = double.Parse(Console.ReadLine());
+
+            double Km = milhas * 1857 / 1000;
+
+            Console.WriteLine("Em quilometros: " + Km);
+        }
+    }
+}
