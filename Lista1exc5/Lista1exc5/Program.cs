b596dc516223f19f417628555bcbf43f86316a0b
LeticiaMouraLM <lehs5900@gmail.com>
2026-04-23 22:14:15 -0300

    Add files via upload

---
+++ Lista1exc5/Lista1exc5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1exc5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double soma = 0;
+
+            for (int i = 1; i <= 4; i++) 
+
+           Console.Write("Digite o valor " + i + ": ");
+            soma += double.Parse(Console.ReadLine());
+
+            double media = soma / 4;
+            
+            Console.WriteLine("media:" + media);
+
+        }
+    }
+}
