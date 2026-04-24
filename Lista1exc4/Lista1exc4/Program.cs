e57dce06a903b3c92bc7daa874a616b22128ec80
LeticiaMouraLM <lehs5900@gmail.com>
2026-04-23 22:06:24 -0300

    Add files via upload

---
+++ Lista1exc4/Lista1exc4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1exc4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(" Base: ");
+            double b = double.Parse(Console.ReadLine());
+
+            Console.Write("Altura: ");
+            double h = double.Parse(Console.ReadLine());
+
+            double area = (b * h) / 2;
+
+            Console.WriteLine("Area do triangulo: " + area);
+
+        }
+    }
+}
