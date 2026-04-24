decaeac2ee95d4c2f82ea865b945235892f0e874
LeticiaMouraLM <lehs5900@gmail.com>
2026-04-24 16:24:13 -0300

    Add files via upload

---
+++ Lista 1 exc 12/Lista 1 exc 12/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista_1_exc_12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         double soma= 0;
+
+         for (int i = 1; i <= 5; i++)
+                
+         Console.Write("Valor do produto " + i + ": ");
+         soma += double.Parse(Console.ReadLine());
+
+         Console.Write("Valor pago: ");
+         double pago = double.Parse(Console.ReadLine());    
+
+         double troco = pago - soma;
+
+         Console.WriteLine("Troco: R$" + troco);
+
+
+
+        }
+    }
+}
