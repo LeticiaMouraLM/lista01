e88dcfebd44476f581f9f25942a9bcbf4f5497cd
LeticiaMouraLM <lehs5900@gmail.com>
2026-04-23 22:46:41 -0300

    Add files via upload

---
+++ Lista 1 exc10/Lista 1 exc10/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista_1_exc10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("cotação do dolar:");
+            double cotação = double.Parse(Console.ReadLine());
+
+            Console.Write("Quantidade em dólares: ");
+            double dolares = double.Parse(Console.ReadLine());
+
+            double reais = cotação * dolares;
+
+            Console.WriteLine("Valor em reais: R$ " + reais);
+        }
+    }
+}
